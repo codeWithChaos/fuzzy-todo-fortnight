e58be7ed591445be37b134985940dbe4d2b35143
codewithchaos <codewithchaos1809@gmail.com>
2024-10-29 10:57:47 +0000

    Initial commit.

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class Todo(models.Model):
     name = models.CharField(max_length=100)
-    body = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Todo'
     
     def __str__(self):
         return self.name
